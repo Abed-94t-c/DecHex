8f247903c5a311f49a46a357cb810c25de55ff00
Abed-94t-c <aaltai300@caledonian.ac.uk>
2024-11-12 07:00:02 +0000

    Replace System.out with Logger to address SonarQube feedback

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,12 +1,13 @@
-import java.util.Scanner;
+import java.util.logging.Logger;
 
 class Dec2Hex {
     public static int Arg1;
+    private static final Logger logger = Logger.getLogger(Dec2Hex.class.getName());
 
     public static void main(String args[]) {
         // Check if an argument is provided
         if (args.length == 0) {
-            System.out.println("Error: No input argument provided. Please provide an integer.");
+            logger.severe("Error: No input argument provided. Please provide an integer.");
             return; // Exit the program if no argument is given
         }
 
@@ -15,7 +16,7 @@ class Dec2Hex {
             Arg1 = Integer.parseInt(args[0]);
         } catch (NumberFormatException e) {
             // Handle the case where the input is not an integer
-            System.out.println("Error: Invalid input. Please provide a valid integer.");
+            logger.severe("Error: Invalid input. Please provide a valid integer.");
             return; // Exit the program if the input is not an integer
         }
 
@@ -24,7 +25,7 @@ class Dec2Hex {
         int rem, num;
         num = Arg1;
         String hexadecimal = "";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+        logger.info("Converting the Decimal Value " + num + " to Hex...");
 
         while (num != 0) {
             rem = num % 16;
@@ -32,7 +33,7 @@ class Dec2Hex {
             num = num / 16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        logger.info("Hexadecimal representation is: " + hexadecimal);
     }
 }
 
