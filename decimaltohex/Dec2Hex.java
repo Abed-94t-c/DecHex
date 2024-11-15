2eeef4228e2c77e6c6865e5e6261517aec30254d
Abed-94t-c <aaltai300@caledonian.ac.uk>
2024-11-15 03:04:15 +0000

    update java file

--- decimaltohex/Dec2Hex.java
+++ decimaltohex/Dec2Hex.java
@@ -14,7 +14,7 @@ public class Dec2Hex {
         }
 
         // Array for hex characters
-            
+              char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'}; 
         int rem;
         StringBuilder hexadecimal = new StringBuilder();
 
